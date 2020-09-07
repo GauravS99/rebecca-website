6652c8e45662349460c8e0cab6707bf49a184cce
Gaurav Sharma <g.sharma0909@gmail.com>
2020-09-06 22:19:49 -0400

    first version

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,7 +1,20 @@
 const MICROSOFT_IMAGINE_CUP = `
-    <iframe src="https://drive.google.com/file/d/1GJT34QO9p1U8PITaw0zBqYy6kktNvnm7/preview" width="1800px" height="1000"></iframe>
+    <iframe class="d-block content-width" src="https://drive.google.com/file/d/1GJT34QO9p1U8PITaw0zBqYy6kktNvnm7/preview"height="1000"></iframe>
 `
 
 const ENSO = `
+    <img src="assets/enso.jpg"  alt="grocer" width="900">
+`
 
+const GOOGLE = `
+    <img src="assets/Google.jpg" alt="grocer" width="900">
+`
+
+const RESEARCH = `
+    <img  class="d-block content-width mb-5" src="assets/Research.jpg" alt="research">
+    <iframe class="d-block content-width" src="https://drive.google.com/file/d/1VvFKCqVIu9BY60_NmAlAVVmnp-8SL75X/preview"  height="1000"></iframe>
+`
+
+const ANDROID = `
+    <iframe class="d-block content-width" src="https://drive.google.com/file/d/1p-B3SNTa3OLbf1ZtGqkmsktLrLhb9-x-/preview" height="1000"></iframe>  
 `
--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const parentFirstProject = document.getElementById("firstProject")
     parentFirstProject.innerHTML = firstProject.trim();
 
-    document.getElementById("timeline-1").addEventListener("click", () => {
-        
-    })
-
     document.getElementById("timeline-2").addEventListener("click", () => {
-        
+        const elem = document.getElementById("selectedProject")
+        elem.innerHTML = MICROSOFT_IMAGINE_CUP
     })
 
     document.getElementById("timeline-3").addEventListener("click", () => {
-        
+        const elem = document.getElementById("selectedProject")
+        elem.innerHTML = ENSO
+    })
+
+    document.getElementById("timeline-4").addEventListener("click", () => {
+        const elem = document.getElementById("selectedProject")
+        elem.innerHTML = GOOGLE
+    })
+
+    document.getElementById("timeline-5").addEventListener("click", () => {
+        const elem = document.getElementById("selectedProject")
+        elem.innerHTML = RESEARCH
+    })
+
+    document.getElementById("timeline-6").addEventListener("click", () => {
+        const elem = document.getElementById("selectedProject")
+        elem.innerHTML = ANDROID
     })
 
 });
