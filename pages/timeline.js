411069915055f7b2561871509a2373648855a9e3
Gaurav Sharma <g.sharma0909@gmail.com>
2020-09-06 21:25:45 -0400

    initial commit

---
+++ pages/timeline.js
@@ -0,0 +1,7 @@
+const MICROSOFT_IMAGINE_CUP = `
+    <iframe src="https://drive.google.com/file/d/1GJT34QO9p1U8PITaw0zBqYy6kktNvnm7/preview" width="1800px" height="1000"></iframe>
+`
+
+const ENSO = `
+
+`
---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", (event) => { 
+    const parentFirstProject = document.getElementById("firstProject")
+    parentFirstProject.innerHTML = firstProject.trim();
+
+    document.getElementById("timeline-1").addEventListener("click", () => {
+        
+    })
+
+    document.getElementById("timeline-2").addEventListener("click", () => {
+        
+    })
+
+    document.getElementById("timeline-3").addEventListener("click", () => {
+        
+    })
+
+});
